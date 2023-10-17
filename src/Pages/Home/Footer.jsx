09df0d5ac85e24ccbfd8b9699eb0c7f88edb2293
“wilvargas” <wilvargas7gmail.com>
2023-10-17 04:14:55 -0500

    Mejoras en el contenido, aun no he podido recuperar la imagen del header

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -172,7 +172,7 @@ function Footer() {
       </div>
       <hr className="divider" />
       <div className="footer--content--container">
-        <p className="footer--content">Made with 💖 by Will</p>
+        <p className="footer--content">Made with heart by Will</p>
         <div className="footer--social--icon">
           <ul>
             <li>
--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -10,6 +10,7 @@ export default function MyPortfolio() {
         </div>
         <div>
           <button className="btn btn-github">
+          <a href="https://github.com/Wilvargas/portfolio-wilvav" className="btn btn-github">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
@@ -25,42 +26,47 @@ export default function MyPortfolio() {
               />
             </svg>
             Visit My GitHub
+            </a>
           </button>
         </div>
       </div>
       <div className="portfolio--section--container">
-        {data?.portfolio?.map((item, index) => (
-          <div key={index} className="portfolio--section--card">
-            <div className="portfolio--section--img">
-              <img src={item.src} alt="Placeholder" />
-            </div>
-            <div className="portfolio--section--card--content">
-              <div>
-                <h3 className="portfolio--section--title">{item.title}</h3>
-                <p className="text-md">{item.description}</p>
-              </div>
-              <p className="text-sm portfolio--link">
-                {item.link}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 20 19"
-                  fill="none"
-                >
-                  <path
-                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
-                    stroke="currentColor"
-                    stroke-width="2.66667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </p>
-            </div>
-          </div>
-        ))}
+  {data?.portfolio?.map((item, index) => (
+    <div key={index} className="portfolio--section--card">
+      <div className="portfolio--section--img">
+        <img src={item.src} alt="Placeholder" />
       </div>
+      <div className="portfolio--section--card--content">
+        <div>
+          <h3 className="portfolio--section--title">{item.title}</h3>
+          <p className="text-md">{item.description}</p>
+        </div>
+        <p className="text-sm portfolio--link">
+          <a href={item.link}>
+          Visit Website...
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 20 19"
+              fill="none"
+            >
+              <path
+                d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                stroke="currentColor"
+                stroke-width="2.66667"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            
+          </a>
+        </p>
+      </div>
+    </div>
+  ))}
+</div>
+
     </section>
   );
 }
